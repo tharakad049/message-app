a0afb61874f621dfd5ed321279dca90849c9c845
DilanTharaka <tharakad049@gmail.com>
2022-08-15 20:14:04 +0530

    update  project

---
+++ src/controller/ChatRoomBoxController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class ChatRoomBoxController {
+    public Label clientName;
+    public TextArea msgRoom;
+    public TextField msgField;
+    public Pane chat;
+
+    public void sendMessageByKey(KeyEvent keyEvent) {
+    }
+
+    public void handleSendEvent(MouseEvent mouseEvent) {
+    }
+
+    public void selectImage(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/controller/LoginAndRegFormController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+
+public class LoginAndRegFormController {
+    public Pane pnSignUp;
+    public TextField txtRegname;
+    public TextField txtRegEmail;
+    public TextField txtRegPassword;
+    public TextField txtRegPhoneNo;
+    public Button getStarted;
+    public RadioButton male;
+    public ToggleGroup Gender;
+    public RadioButton female;
+    public TextField txtRegFirstname;
+    public Label controlRegLabel;
+    public Label success;
+    public Label goBack;
+    public Pane pnSignIn;
+    public Label loginNotifier;
+    public Button btnSignUp;
+    public PasswordField txtPassword;
+    public TextField txtUsername;
+
+    public void forgetPassword(ActionEvent actionEvent) {
+    }
+
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void handleButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void registration(ActionEvent actionEvent) {
+    }
+}
